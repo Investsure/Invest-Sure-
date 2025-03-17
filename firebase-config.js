59b2463f1ee65e9a75a52f2331dcdfd1d66e05b3
Investsure <falcongamingyt154@gmail.com>
2025-03-17 16:03:03 +0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXrrmPtO784Iy3QnHq5BR6ytJGPruzotY",
+    authDomain: "investsure-3a827.firebaseapp.com",
+    projectId: "investsure-3a827",
+    storageBucket: "investsure-3a827.firebasestorage.app",
+    messagingSenderId: "23509833134",
+    appId: "1:23509833134:web:86362cdd2f52f730c8a43f"
+};
 
+// Firebase ইন্সট্যান্স ইনিশিয়ালাইজ করা
+firebase.initializeApp(firebaseConfig);
