3d6cdecd646adc93eb51c24ddbe42123ff7c7e5e
Investsure <falcongamingyt154@gmail.com>
2025-03-17 16:12:29 +0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,20 @@
+// Firebase SDK থেকে প্রয়োজনীয় ফাংশন ইম্পোর্ট করুন
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+// Firebase কনফিগারেশন সেটআপ
 const firebaseConfig = {
-    apiKey: "AIzaSyDXrrmPtO784Iy3QnHq5BR6ytJGPruzotY",
-    authDomain: "investsure-3a827.firebaseapp.com",
-    projectId: "investsure-3a827",
-    storageBucket: "investsure-3a827.firebasestorage.app",
-    messagingSenderId: "23509833134",
-    appId: "1:23509833134:web:86362cdd2f52f730c8a43f"
+  apiKey: "AIzaSyDXrrmPtO784Iy3QnHq5BR6ytJGPruzotY",
+  authDomain: "investsure-3a827.firebaseapp.com",
+  projectId: "investsure-3a827",
+  storageBucket: "investsure-3a827.appspot.com",
+  messagingSenderId: "23509833134",
+  appId: "1:23509833134:web:86362cdd2f52f730c8a43f"
 };
 
-// Firebase ইন্সট্যান্স ইনিশিয়ালাইজ করা
-firebase.initializeApp(firebaseConfig);
+// Firebase ইনিশিয়ালাইজ করুন
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// এক্সপোর্ট করুন যাতে অন্য ফাইল থেকে এক্সেস করা যায়
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
