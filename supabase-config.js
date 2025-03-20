3db4ad693245057ed51b233c8bc9813c1f4ec54d
Investsure <falcongamingyt154@gmail.com>
2025-03-20 12:40:11 +0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
 
+// Supabase Connection Setup
+const supabaseUrl = 'https://pbnlcbnxzjgeflozxwbt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBibmxjYm54empnZWZsb3p4d2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNjU3NTAsImV4cCI6MjA1Nzg0MTc1MH0.sZylRRmtx88t0d2x40yhgQrvWDAwPimBxtsKgDE_EpQ'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export { supabase }
